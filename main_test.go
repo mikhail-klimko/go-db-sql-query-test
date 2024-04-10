e2b792929be42566f0ac5a8c9bd1ead5445efe60
mikhail-klimko <mklimko123@gmail.com>
2024-04-10 14:47:48 +0300

    wip: Wed Apr 10 14:47:48 +03 2024

--- main_test.go
+++ main_test.go
@@ -1,29 +1,72 @@
 package main
 
 import (
+	"database/sql"
 	"testing"
 
 	_ "modernc.org/sqlite"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_SelectClient_WhenOk(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer db.Close()
 
 	clientID := 1
+	client := Client{}
+	client, err = selectClient(db, clientID)
+
+	require.NoError(t, err)
+	require.Equal(t, clientID, client.ID)
+	assert.NotEmpty(t, client.FIO)
+	assert.NotEmpty(t, client.Login)
+	assert.NotEmpty(t, client.Birthday)
+	assert.NotEmpty(t, client.Email)
 
 	// напиши тест здесь
 }
 
 func Test_SelectClient_WhenNoClient(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer db.Close()
 
 	clientID := -1
+	client := Client{}
+	client, err = selectClient(db, clientID)
+	require.ErrorIs(t, err, sql.ErrNoRows)
+
+	assert.Empty(t, client.ID)
+	assert.Empty(t, client.FIO)
+	assert.Empty(t, client.Login)
+	assert.Empty(t, client.Birthday)
+	assert.Empty(t, client.Email)
 
 	// напиши тест здесь
 }
 
 func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer db.Close()
 
 	cl := Client{
 		FIO:      "Test",
@@ -33,10 +76,24 @@ func Test_InsertClient_ThenSelectAndCheck(t *testing.T) {
 	}
 
 	// напиши тест здесь
+	cl.ID, err = insertClient(db, cl)
+	require.NoError(t, err)
+	require.NotEmpty(t, cl.ID)
+
+	stored, err := selectClient(db, cl.ID)
+	require.NoError(t, err)
+	assert.Equal(t, cl, stored)
 }
 
 func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
 	// настройте подключение к БД
+	db, err := sql.Open("sqlite", "demo.db")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer db.Close()
 
 	cl := Client{
 		FIO:      "Test",
@@ -46,4 +103,17 @@ func Test_InsertClient_DeleteClient_ThenCheck(t *testing.T) {
 	}
 
 	// напиши тест здесь
+	cl.ID, err = insertClient(db, cl)
+	require.NoError(t, err)
+	require.NotEmpty(t, cl.ID)
+
+	stored, err := selectClient(db, cl.ID)
+	require.NoError(t, err)
+	assert.Equal(t, cl, stored)
+
+	deleteClient(db, cl.ID)
+	require.NoError(t, err)
+
+	_, err = selectClient(db, cl.ID)
+	require.ErrorIs(t, err, sql.ErrNoRows)
 }
